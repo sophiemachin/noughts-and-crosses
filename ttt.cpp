3881ffb8ed8008deca2272489d14ae177db52757
Sophie Machin <sophiemachin@gmail.com>
2021-08-28 09:40:15 +0100

    working

--- ttt.cpp
+++ ttt.cpp
@@ -10,8 +10,24 @@ using namespace std;
 // g++ -std=c++11 ttt.cpp && ./a.out
 
 int board[9] = { 0 };
+int sums[9] = { 8, 1, 6, 3, 5, 7, 4, 9, 2 };
+
+int winningCombos[8][3] = { 
+	// rows
+	{ 0, 1, 2 },
+	{ 3, 4, 5 },
+	{ 6, 7, 8 },
+	// columns
+	{ 0, 3, 6 },
+	{ 1, 4, 7 },
+	{ 2, 5, 8 },
+	// diagonals
+	{ 0, 4, 8 },
+	{ 2, 4, 6 },
+};
 
 bool winnerDeclared = false;
+int winner = 0;
 
 map<int,string> m = {
     { 0, " " },
@@ -27,7 +43,7 @@ map<bool,string> mark = {
 
 void draw() {
 
-	cout << "\033[2J\033[1;1H"; // clear the screen so the board is always drawn at the top of the terminal
+	// cout << "\033[2J\033[1;1H"; // clear the screen so the board is always drawn at the top of the terminal
 
     cout << "\n     |     |      \n";
 
@@ -48,8 +64,29 @@ void draw() {
 
 }
 
-bool isWinner() {
-	return (board[0] == board[3] == board[6] && board[0]);
+int sumLine(int a, int b, int c, bool aF, bool bF, bool cF) {
+	return sums[a]*aF + sums[b]*bF + sums[c]*cF;
+}
+
+int isWinningRow(int a, int b, int c) {
+	return (sumLine(a, b, c, board[a], board[b], board[c]) == 15) && 
+		((board[a] == board[b]) && (board[a] == board[c]));
+}
+
+int getWinner(int a, int b, int c) {
+	if (isWinningRow(a, b, c)) {
+		int sum = board[a] + board[b] + board[c];
+		if (sum == 6) 
+			return 2;
+		else if (sum == 3)
+			return 1;
+	}
+	return 0;
+}
+
+void printTurnInfo(int turn, int x) {
+	cout << "Turn: " << turn << "\n";
+	cout << "Player " << x << "'s turn\n";
 }
 
 
@@ -58,11 +95,12 @@ int main () {
 	draw();
 
 	int position;
-	int x = 1;
+	int x = 0;
+	int turn = 1;
+
 
 	while (!winnerDeclared) {
-
-		cout << "Player " << x << "'s turn\n";
+		printTurnInfo(turn, x);
 
 		int validPosition = false;
 
@@ -78,15 +116,21 @@ int main () {
 		
 		board[position] = x + 1;
 
-		cout << isWinner();
-
 		draw();
 
-		cout << "isWinner: " << isWinner();
+		for (auto c : winningCombos) {
+			winner = getWinner(c[0], c[1], c[2]);
+
+			if (winner) 
+				winnerDeclared = true;
+		}
 
 		x = 1 - x;
 
+		turn++;
+
 	}
 
+	cout << "Player " << winner << " wins";
 	cout << "\n\n";
 }
